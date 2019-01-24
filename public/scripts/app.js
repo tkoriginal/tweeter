09476748e5b54d210bda922535eb2783b6e5225a
tkorignal <tauqueer.khan@gmail.com>
2019-01-24 18:12:07 +0000

    Updated error display

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,54 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
 
 function renderTweets(tweets) {
+  $('.tweet-container').empty();
   tweets.forEach(tweet => {
     const $tweetBlock = createTweetElement(tweet);
     $('.tweet-container'). prepend($tweetBlock);
@@ -67,7 +22,7 @@ const createTweetElement = (tweetObj) => {
       <h2 class="tweet__name">${tweetObj.user.name}</h2>
       <p class="tweet__handle">${tweetObj.user.handle}</p>
     </header>
-    <p class="tweet__text">${tweetObj.content.text}</p>
+    <p class="tweet__text">${escape(tweetObj.content.text)}</p>
     <footer class="tweet__footer">
       <p class="tweet__posted">${moment(tweetObj.created_at).fromNow()}</p>
       <div class="tweet__icon">
@@ -80,6 +35,12 @@ const createTweetElement = (tweetObj) => {
   )
 }
 
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+ 
 const loadTweets = () => {
   $.get('/tweets', (data, status) => {
     if (status === 'success'){
@@ -106,18 +67,29 @@ const newTweet = () => {
     const data = form.serialize();
     const textArea = form[0][0]
     if (isInvalidMessage(textArea.value)){
-      alert(isInvalidMessage(textArea.value));
+      setTimeout($('.new-error').text(isInvalidMessage(textArea.value)), 3000);
+      $('.new-error').slideDown('medium');
     } else {
+      $('.new-error').slideUp()
       $.post('/tweets',
         data, 
         (data) => {
           loadTweets();
           $(textArea).val('');
+          $('.counter').text(140)
         });
       }
     })
 }
+
+$('.nav-bar__compose').on('click', () =>{
+  $('.new-tweet').slideToggle('medium')
+  $('.textarea').focus();
+})
+
 $(document).ready(function() {
+  $('.new-error').hide()
   loadTweets();
   newTweet();
+  
 })
